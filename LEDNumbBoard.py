9f0941bc1881a97f2cc55bc2f174062a5fa48f38
JAYASRI-SRIDHAR <51885271+JAYASRI-SRIDHAR@users.noreply.github.com>
2024-03-28 13:56:14 +0530

    Create LEDNumbBoard.py
    Write a program which is able to simulate the work of a seven-display device, although you're going to use single LEDs instead of segments.
    
    Each digit is constructed from 13 LEDs (some lit, some dark, of course) 
    
    Your code has to display any non-negative integer number entered by the user.
    
    Tip: using a list containing patterns of all ten digits may be very helpful.
    
    Test data
    Sample input:
    
    123
    
    Sample output:
    
      # ### ### 
      #     #     # 
      # ### ### 
      # #         # 
      # ### ###

---
+++ LEDNumbBoard.py
@@ -0,0 +1,232 @@
+#Not an effecient way.
+def line1(numb,cnt):
+    for i in range(0,cnt):
+        if i==cnt-1:
+            if(numb[i])=='1':
+                print("#")
+            elif(numb[i])=='2':
+                print("###")
+            elif(numb[i])=='3':
+                print("###")                
+            elif(numb[i])=='4':
+                print("# #")    
+            elif(numb[i])=='5':
+                print("###")
+            elif(numb[i])=='6':
+                print("###")
+            elif(numb[i])=='7':
+                print("###")
+            elif(numb[i])=='8':
+                print("###")
+            elif(numb[i])=='9':
+                print("###")
+            elif(numb[i])=='0':
+                print("###")
+        else:
+            if(numb[i])=='1':
+                print("#", end=' ')
+            elif(numb[i])=='2':
+                print("###", end=' ')
+            elif(numb[i])=='3':
+                print("###", end=' ')                
+            elif(numb[i])=='4':
+                print("# #", end=' ')    
+            elif(numb[i])=='5':
+                print("###", end=' ')
+            elif(numb[i])=='6':
+                print("###", end=' ')
+            elif(numb[i])=='7':
+                print("###", end=' ')
+            elif(numb[i])=='8':
+                print("###", end=' ')
+            elif(numb[i])=='9':
+                print("###", end=' ')
+            elif(numb[i])=='0':
+                print("###", end=' ')
+def line2(numb,cnt):
+    for i in range(0,cnt):
+        if i==cnt-1:
+            if(numb[i])=='1':
+                print("#")
+            elif(numb[i])=='2':
+                print("  #")
+            elif(numb[i])=='3':
+                print("  #")                
+            elif(numb[i])=='4':
+                print("# #")    
+            elif(numb[i])=='5':
+                print("#  ")
+            elif(numb[i])=='6':
+                print("#  ")
+            elif(numb[i])=='7':
+                print("  #")
+            elif(numb[i])=='8':
+                print("# #")
+            elif(numb[i])=='9':
+                print("# #")
+            elif(numb[i])=='0':
+                print("# #")
+        else:
+            if(numb[i])=='1':
+                print("#", end=' ')
+            elif(numb[i])=='2':
+                print("  #", end=' ')
+            elif(numb[i])=='3':
+                print("  #", end=' ')                
+            elif(numb[i])=='4':
+                print("# #", end=' ')    
+            elif(numb[i])=='5':
+                print("#  ", end=' ')
+            elif(numb[i])=='6':
+                print("#  ", end=' ')
+            elif(numb[i])=='7':
+                print("  #", end=' ')
+            elif(numb[i])=='8':
+                print("# #", end=' ')
+            elif(numb[i])=='9':
+                print("# #", end=' ')
+            elif(numb[i])=='0':
+                print("# #", end=' ')
+def line3(numb,cnt):
+    for i in range(0,cnt):
+        if i==cnt-1:
+            if(numb[i])=='1':
+                print("#")
+            elif(numb[i])=='2':
+                print("###")
+            elif(numb[i])=='3':
+                print("###")                
+            elif(numb[i])=='4':
+                print("###")    
+            elif(numb[i])=='5':
+                print("###")
+            elif(numb[i])=='6':
+                print("###")
+            elif(numb[i])=='7':
+                print("  #")
+            elif(numb[i])=='8':
+                print("###")
+            elif(numb[i])=='9':
+                print("###")
+            elif(numb[i])=='0':
+                print("# #")
+        else:
+            if(numb[i])=='1':
+                print("#", end=' ')
+            elif(numb[i])=='2':
+                print("###", end=' ')
+            elif(numb[i])=='3':
+                print("###", end=' ')                
+            elif(numb[i])=='4':
+                print("###", end=' ')    
+            elif(numb[i])=='5':
+                print("###", end=' ')
+            elif(numb[i])=='6':
+                print("###", end=' ')
+            elif(numb[i])=='7':
+                print("  #", end=' ')
+            elif(numb[i])=='8':
+                print("###", end=' ')
+            elif(numb[i])=='9':
+                print("###", end=' ')
+            elif(numb[i])=='0':
+                print("# #", end=' ')
+def line4(numb,cnt):
+    for i in range(0,cnt):
+        if i==cnt-1:
+            if(numb[i])=='1':
+                print("#")
+            elif(numb[i])=='2':
+                print("#  ")
+            elif(numb[i])=='3':
+                print("  #")                
+            elif(numb[i])=='4':
+                print("  #")    
+            elif(numb[i])=='5':
+                print("  #")
+            elif(numb[i])=='6':
+                print("# #")
+            elif(numb[i])=='7':
+                print("  #")
+            elif(numb[i])=='8':
+                print("# #")
+            elif(numb[i])=='9':
+                print("  #")
+            elif(numb[i])=='0':
+                print("# #")
+        else:
+            if(numb[i])=='1':
+                print("#", end=' ')
+            elif(numb[i])=='2':
+                print("#  ", end=' ')
+            elif(numb[i])=='3':
+                print("  #", end=' ')                
+            elif(numb[i])=='4':
+                print("  #", end=' ')    
+            elif(numb[i])=='5':
+                print("  #", end=' ')
+            elif(numb[i])=='6':
+                print("# #", end=' ')
+            elif(numb[i])=='7':
+                print("  #", end=' ')
+            elif(numb[i])=='8':
+                print("# #", end=' ')
+            elif(numb[i])=='9':
+                print("  #", end=' ')
+            elif(numb[i])=='0':
+                print("# #", end=' ')
+def line5(numb,cnt):
+    for i in range(0,cnt):
+        if i==cnt-1:
+            if(numb[i])=='1':
+                print("#")
+            elif(numb[i])=='2':
+                print("###")
+            elif(numb[i])=='3':
+                print("###")                
+            elif(numb[i])=='4':
+                print("  #")    
+            elif(numb[i])=='5':
+                print("###")
+            elif(numb[i])=='6':
+                print("###")
+            elif(numb[i])=='7':
+                print("  #")
+            elif(numb[i])=='8':
+                print("###")
+            elif(numb[i])=='9':
+                print("###")
+            elif(numb[i])=='0':
+                print("###")
+        else:
+            if(numb[i])=='1':
+                print("#", end=' ')
+            elif(numb[i])=='2':
+                print("###", end=' ')
+            elif(numb[i])=='3':
+                print("###", end=' ')                
+            elif(numb[i])=='4':
+                print("  #", end=' ')    
+            elif(numb[i])=='5':
+                print("###", end=' ')
+            elif(numb[i])=='6':
+                print("###", end=' ')
+            elif(numb[i])=='7':
+                print("  #", end=' ')
+            elif(numb[i])=='8':
+                print("###", end=' ')
+            elif(numb[i])=='9':
+                print("###", end=' ')
+            elif(numb[i])=='0':
+                print("###", end=' ')
+num=int(input("Enter your number: "))
+if (num>=0):
+    numb=str(num)
+    cnt=len(numb)
+    line1(numb,cnt)
+    line2(numb,cnt)
+    line3(numb,cnt)
+    line4(numb,cnt)
+    line5(numb,cnt)
+else:
+    print("NAN")
